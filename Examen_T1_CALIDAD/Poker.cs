b5606ace44e48619207f679b33c44dcbfdf47e5b
Elard Burga Goicochea <elard3d@hotmail.com>
2021-09-11 00:06:19 -0500

    validarJugadores, validarCartas, validarCartasIguales

--- Examen_T1_CALIDAD/Poker.cs
+++ Examen_T1_CALIDAD/Poker.cs
@@ -1,14 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Examen_T1_CALIDAD
 {
     public class Poker
     {
-        public void jugar(Array jugadores, Array cartas) 
+        public string validarJugadores(Array jugadores, Array cartas)
         {
-            return;
+
+            if (jugadores.Length < 2)
+            {
+
+                return "jugadores no aceptados";
+
+            }
+            else
+            {
+                return "jugadores aceptados";
+
+            }
+
+
+
+        }
+
+        public string validarCartas(Array jugadores, Array cartas)
+        {
+
+            if (jugadores.Length * 5 == cartas.Length)
+            {
+
+                return "cartas exactas";
+
+            }
+            else
+            {
+
+                return "cartas error";
+
+            }
+
+
+        }
+
+        public string validarCartasIguales(Array jugadores, List<String> cartas)
+        {
+
+            var duplicados = 0;
+
+            foreach (var grouping in cartas.GroupBy(t => t).Where(t => t.Count() != 1))
+            {
+             
+                duplicados += grouping.Count();
+
+            }
+
+            if (duplicados >= 1)
+            {
+                return "cartas duplicadas";
+            }
+            else
+            {
+                return "cartas sin duplicar";
+            }
+
+
         }
     }
 }
--- Examen_T1_CALIDAD_TESTING/UnitTest1.cs
+++ Examen_T1_CALIDAD_TESTING/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Examen_T1_CALIDAD;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Examen_T1_CALIDAD_TESTING
 {
@@ -7,16 +8,146 @@ namespace Examen_T1_CALIDAD_TESTING
     {
 
         [Test]
-        public void Test1()
+
+        public void CASO1()
         {
             Poker poker = new Poker();
 
             string [] jugadores = new string[] { "Jugador1", "Jugador2" , "Jugador3" };
 
+            string[] cartas = new string[] { "Carta1", "Carta1", "Carta1", 
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1"};
 
-           
+            var resultadoEsperado = "jugadores aceptados";
+            var resultadoReal = poker.validarJugadores(jugadores, cartas);
 
-            poker.jugar();
+            Assert.AreEqual(resultadoReal, resultadoEsperado);
+
         }
+
+        [Test]
+        public void CASO2()
+        {
+            Poker poker = new Poker();
+
+            string[] jugadores = new string[] { "Jugador1"};
+
+            string[] cartas = new string[] { "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1" };
+
+            var resultadoEsperado = "jugadores no aceptados";
+            var resultadoReal = poker.validarJugadores(jugadores, cartas);
+
+            Assert.AreEqual(resultadoReal, resultadoEsperado);
+
+        }
+
+        [Test]
+
+        public void CASO3()
+        {
+            Poker poker = new Poker();
+
+            string[] jugadores = new string[] { "Jugador1", "Jugador2"};
+
+            string[] cartas = new string[] { "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1"};
+
+            var resultadoEsperado = "jugadores aceptados";
+            var resultadoReal = poker.validarJugadores(jugadores, cartas);
+
+            Assert.AreEqual(resultadoReal, resultadoEsperado);
+
+        }
+
+        [Test]
+
+        public void CASO4()
+        {
+            Poker poker = new Poker();
+
+            string[] jugadores = new string[] { "Jugador1", "Jugador2" };
+
+            string[] cartas = new string[] { "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1","Carta1","Carta1"};
+
+            var resultadoEsperado = "cartas error";
+            var resultadoReal = poker.validarCartas(jugadores, cartas);
+
+            Assert.AreEqual(resultadoReal, resultadoEsperado);
+
+        }
+
+        [Test]
+        public void CASO5()
+        {
+            Poker poker = new Poker();
+
+            string[] jugadores = new string[] { "Jugador1", "Jugador2" , "Jugador3" };
+
+            string[] cartas = new string[] { "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1","Carta1","Carta1",
+                                            "Carta1","Carta1", "Carta1"};
+
+            var resultadoEsperado = "cartas exactas";
+            var resultadoReal = poker.validarCartas(jugadores, cartas);
+
+            Assert.AreEqual(resultadoReal, resultadoEsperado);
+
+        }
+
+        [Test]
+        public void CASO6()
+        {
+            Poker poker = new Poker();
+
+            string[] jugadores = new string[] { "Jugador1", "Jugador2", "Jugador3" };
+
+            List<string> cartas = new List<string>()
+                                            { "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1", "Carta1", "Carta1",
+                                            "Carta1","Carta1","Carta1",
+                                            "Carta1","Carta1", "Carta1"};
+
+            var resultadoEsperado = "cartas duplicadas";
+            var resultadoReal = poker.validarCartasIguales(jugadores, cartas);
+
+            Assert.AreEqual(resultadoReal, resultadoEsperado);
+
+        }
+
+        [Test]
+        public void CASO7()
+        {
+            Poker poker = new Poker();
+
+            string[] jugadores = new string[] { "Jugador1", "Jugador2", "Jugador3" };
+
+            List<string> cartas = new List<string>()
+                                            { "Carta1", "Carta2", "Carta3",
+                                            "Carta4", "Carta5", "Carta6",
+                                            "Carta7", "Carta8", "Carta9",
+                                            "Carta10","Carta11","Carta12",
+                                            "Carta13","Carta14", "Carta15"};
+
+            var resultadoEsperado = "cartas sin duplicar";
+            var resultadoReal = poker.validarCartasIguales(jugadores, cartas);
+
+            Assert.AreEqual(resultadoReal, resultadoEsperado);
+
+        }
+
+
+
     }
 }
