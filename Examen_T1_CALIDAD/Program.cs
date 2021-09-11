f8c96a4e0ef5aa1a5b07504a579931216b82803c
Elard Burga Goicochea <elard3d@hotmail.com>
2021-09-10 20:58:22 -0500

    Agregar archivos de proyecto.

---
+++ Examen_T1_CALIDAD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examen_T1_CALIDAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
