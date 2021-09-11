a435b998fa79e0a4b50250eb55dd31013945e525
Elard Burga Goicochea <elard3d@hotmail.com>
2021-09-10 22:28:12 -0500

    Se creó testing

---
+++ Examen_T1_CALIDAD_TESTING/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Examen_T1_CALIDAD_TESTING
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
